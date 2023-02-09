8dac3f18f08595d5990cae1dcc5fc8f26e562861
Марко М. Костић (Marko M. Kostić) <marko.m.kostic@gmail.com>
2023-02-09 09:03:29 +0100

    Add the subnets

--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,3 +1,6 @@
 region      = "eu-central-1"
 environment = "dev"
 vpc_cidr    = "10.0.0.0/16"
+mgmt_cidr   = "10.0.254.0/24"
+data_cidr   = "10.0.0.0/20"
+app_cidr    = "10.0.16.0/20"
--- infra/variables.tf
+++ infra/variables.tf
@@ -15,3 +15,18 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "mgmt_cidr" {
+  description = "CIDR used for the VPN management nodes."
+  type        = string
+}
+
+variable "data_cidr" {
+  description = "CIDR used for the data persistence service (Postgres, Kafka)."
+  type        = string
+}
+
+variable "app_cidr" {
+  description = "CIDR used for the applications."
+  type        = string
+}
