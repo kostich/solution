d40a8266d02586b32f477f4a4240986ad865afed
Марко М. Костић (Marko M. Kostić) <marko.m.kostic@gmail.com>
2023-02-11 19:45:08 +0100

    Note about Kafka

---
+++ infra/kafka.tf
@@ -0,0 +1,2 @@
+# TODO: implement https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/msk_cluster
+# Make sure to configure it the same way Postgres is configured (ie. multi-AZ and so on).
